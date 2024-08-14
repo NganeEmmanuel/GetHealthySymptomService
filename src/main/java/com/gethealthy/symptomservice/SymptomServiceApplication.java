f411a47f45e445535bd11567b03d882bb6237c67
Ngane Emmanuel <emmanuelngane06@gmail.com>
2024-08-14 08:23:19 -0700

    initial commit

---
+++ src/main/java/com/gethealthy/symptomservice/SymptomServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gethealthy.symptomservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymptomServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SymptomServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gethealthy/symptomservice/SymptomServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gethealthy.symptomservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymptomServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
